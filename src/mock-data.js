e5490adede91e3ba539ec400e42ceafa3a6e5757
McKayla Pressler <mpressler2021@gmail.com>
2022-03-11 16:28:08 -0700

    added mock data

---
+++ src/mock-data.js
@@ -0,0 +1,153 @@
+let trivia = [{
+    statement: "Cynophobia is the fear of pine trees.",
+    answer: false,
+    info: "Cynophobia is the fear of dogs."
+},
+{
+    statement: "Henry VIII officially made Valentine's Day a holiday in 1537.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "The rarest M&M color is white.",
+    answer: false,
+    info: "The rarest M&M color is brown."
+},
+{
+    statement: "The United States consumes the most chocolate per capita.",
+    answer: false,
+    info: "Switzerland consumes the most chocolate per capita."
+},
+{
+    statement: "The unicorn is the national animal of Scotland.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "Hippopotamus means \"river horse\".",
+    answer: true,
+    info: ""
+},
+{
+    statement: "A group of ravens is known as an unkindness.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "The folds of skin on a cat\'s ears are called \"Henry\'s pockets\".",
+    answer: true,
+    info: ""
+},
+{
+    statement: "A duel between three people is called a truel.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "In the state of Georgia, it\'s illegal to eat fried chicken with a fork.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "Iceland diverted roads to avoid disturbing communities of elves.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "In public places in Florida, it is illegal to sing while wearing a swimsuit.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "William Shakespeare invented the word \"vomit\".",
+    answer: true,
+    info: ""
+},
+{
+    statement: "New Zealand has a ninety mile long beach called \"Ninety Mile Beach\".",
+    answer: false,
+    info: "The Ninety Mile Beach is only 55 miles long."
+},
+{
+    statement: "Muddy York is a nickname for New York in the winter.",
+    answer: false,
+    info: "Muddy York is an old nickname given to Toronto, originally called Town of York."
+},
+{
+    statement: "SWOT analysis is a type of personality test used to screen job applicants.",
+    answer: false,
+    info: "SWOT (strengths, weaknesses, opportunities, threats) is a tool used to analyze the state of a business and plan ahead strategically."
+},
+{
+    statement: "There are three rivers in Saudi Arabia.",
+    answer: false,
+    info: "Saudi Arabia is one 17 countries in the world with no rivers flowing through them."
+},
+{
+    statement: "The Great Wall of China is visible from space.",
+    answer: false,
+    info: "This is a myth."
+},
+{
+    statement: "Seahorses have stomachs for the absorption of nutrients from food.",
+    answer: false,
+    info: "Seahorses don't have stomachs, just intestines."
+},
+{
+    statement: "New York is nicknamed the Big Orange.",
+    answer: false,
+    info: "New York is nicknamed the Big Apple."
+},
+{
+    statement: "Scallops don\'t have good vision.",
+    answer: false,
+    info: "Scallops have as many as 100 simple eyes. They are frequently blue."
+},
+{
+    statement: "Nearly three percent of the ice in Antarctic glaciers is penguin urine.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "Apes can\'t laugh.",
+    answer: false,
+    info: "All apes laugh when tickled."
+},
+{
+    statement: "A snail can sleep for three months.",
+    answer: false,
+    info: "A snail can sleep for up to 3 years."
+},
+{
+    statement: "Your nose and sinuses produce almost one liter of mucus a day (which you swallow).",
+    answer: true,
+    info: ""
+},
+{
+    statement: "It takes a sloth two weeks to digest food.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "There is a world championship for throwing mobile phones.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "Bats always turn right when leaving a cave.",
+    answer: false,
+    info: "Bats always turn left when leaving a cave."
+},
+{
+    statement: "A group of jellyfish is called a smack.",
+    answer: true,
+    info: ""
+},
+{
+    statement: "Slugs have six noses.",
+    answer: false,
+    info: "Slugs only have four noses."
+}
+]
+
+export default trivia;
